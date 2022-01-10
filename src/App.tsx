dc8846f2aeefc4c360c32f9bc75154ef7bd40840
Subodh Singh <singh.01.subodh@gmail.com>
2022-01-10 14:04:09 +0530

    added "basic" FEN to Board conversion method

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { DEFAULT_BOARD } from "./chess-board/FEN";
 
 function App() {
+    console.log(DEFAULT_BOARD);
+
     return (
         <div className="App">
             <header className="App-header">
