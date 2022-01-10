458ad313c15067f08595f93806c5e74f063ac131
Ronak Sanpui <ronaksanpui@gmail.com>
2022-01-10 14:10:15 +0530

    started ChessBoard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Board } from "./chess-board/Board";
+import Piece, { Color } from "./chess-board/Pieces";
+
+
+import ChessBoard from "./components/ChessBoard";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload. Noobda
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            {/* <ChessBoard boardObj="hww"  /> */}
         </div>
     );
 }
+
 
 export default App;
---
+++ src/components/ChessBoard.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { Board } from '../chess-board/Board';
+import { type } from 'os';
+
+
+type Props={
+    boardObj: Board
+}
+type State={
+
+}
+
+
+export default class ChessBoard extends Component<Props,State> {
+    render() {
+        return (
+            <div>
+              
+            </div>
+        )
+    }
+}
+
+
+
