c58aa62a372d365d75ee91c0b3dc4b41314c464d
Sourashis Mondal <sourashismondal872001@gmail.com>
2022-01-12 12:24:23 +0530

    app.tsx er START_BOARD_FEN thik korlam

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
 import "./App.css";
+import { START_BOARD_FEN } from "./chess-board/FEN";
 import Game from "./chess-board/Game";
 import ChessGame from "./components/ChessGame";
 
 function App() {
     const [flip, setFlip] = useState(false);
-    const [fen, setFen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
-    const [fromValue, setFromValue] = useState('');
-    
+    const [fen, setFen] = useState(START_BOARD_FEN);
+    const [fromValue, setFromValue] = useState("");
+
     return (
         <div className="App">
             <ChessGame gameObj={new Game(fen)} flipGame={flip} />
             <form>
                 <label>
                     Name:
-                    <input type="text" name="fen" onChange={(event)=>{setFromValue(event.target.value)}}/>
+                    <input
+                        type="text"
+                        name="fen"
+                        onChange={(event) => {
+                            setFromValue(event.target.value);
+                        }}
+                    />
                 </label>
-                <button type="button" onClick={()=>{setFen(fromValue)}}>Update Game</button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        setFen(fromValue);
+                    }}
+                >
+                    Update Game
+                </button>
             </form>
 
             <button
