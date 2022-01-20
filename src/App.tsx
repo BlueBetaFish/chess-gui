1047eefcab75da78d2f45c3e10e59502e8052b84
Ronak Sanpui <ronaksanpui@gmail.com>
2022-01-20 19:30:33 +0530

    refactored App to class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,65 @@
-import { useState } from "react";
+import { type } from "os";
+import { useState, Component } from "react";
 import "./App.css";
 import { START_BOARD_FEN } from "./chess-board/FEN";
 import Game from "./chess-board/Game";
 import ChessGame from "./components/ChessGame";
 
-function App() {
-    const [flip, setFlip] = useState(false);
-    const [formValue, setFormValue] = useState("");
-    const [gameObj, setGameObj] = useState(new Game("8/PPPPPPPP/k7/8/K7/8/pppppppp/8 b - - 0 1"));
 
-    return (
-        <div className="App">
-            <ChessGame gameObj={gameObj} flipGame={flip} />
-            <form>
-                <input
-                    type="text"
-                    name="fen"
-                    onChange={(event) => {
-                        setFormValue(event.target.value);
-                    }}
-                />
-                <button
-                    type="button"
-                    onClick={(event) => {
-                        event.preventDefault()
-                        setGameObj(new Game(formValue))
-                    }}
-                >
-                    Update Game
-                </button>
-                <button
-                    onClick={(event) => {
-                        event.preventDefault()
-                        setFlip(!flip);
-                    }}
-                >
-                    Flip
-                </button>
-            </form>
-
-
-        </div>
-    );
+type AppState = {
+    flip: boolean;
+    formValue: string;
+    gameObj: Game;
 }
 
-export default App;
+export default class App extends Component<any, AppState> {
+    state: AppState = {
+        flip: false,
+        formValue: "",
+        gameObj: new Game("8/PPPPPPPP/k7/8/K7/8/pppppppp/8 b - - 0 1")
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <ChessGame gameObj={this.state.gameObj} flipGame={this.state.flip} />
+                <form>
+                    <input
+                        type="text"
+                        name="fen"
+                        onChange={(event) => {
+                            this.setState({
+                                formValue: event.target.value
+                            })
+                        }}
+                    />
+                    <button
+                        type="button"
+                        onClick={(event) => {
+                            event.preventDefault()
+                            this.setState({
+                                gameObj: new Game(this.state.formValue)
+                            })
+                        }}
+                    >
+                        Update Game
+                    </button>
+                    <button
+                        onClick={(event) => {
+                            event.preventDefault()
+                            this.setState({
+                                flip: !this.state.flip
+                            })
+                        }}
+                    >
+                        Flip
+                    </button>
+                </form>
+
+
+            </div>
+        )
+    }
+}
+
